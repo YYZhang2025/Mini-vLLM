7aa0fb81392b0598950057ed49b1b2958525966f
YYZhang <yuyangzhang2025@gmail.com>
2026-04-03 14:26:01 +0800

    add running code

--- simple_run.py
+++ simple_run.py
@@ -6,12 +6,22 @@ from vllm.engine.llm_engine import LLMEngine
 from vllm.sampling_params import SamplingParams
 
 
+def get_tensor_parallel_size():
+    import torch
+
+    if torch.cuda.is_available():
+        return torch.cuda.device_count()
+    else:
+        return 1
+
+
 def main():
     path = os.path.expanduser("~/model/Qwen3-0.6B/")
     tokenizer = AutoTokenizer.from_pretrained(path, use_fast=True)
+    tensor_parallel_size = get_tensor_parallel_size()
     engine = LLMEngine(
         model=path,
-        tensor_parallel_size=1,
+        tensor_parallel_size=tensor_parallel_size,
         max_model_len=2048,
     )
 
